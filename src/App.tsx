a0543e17133ebb8b283a7b241419f634013a50f7
Christian Eberling <christian.eberling@logmein.com>
2021-11-11 16:07:07 +0100

    show local vid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [stream, setStream] = useState<MediaStream | undefined>(undefined);
+
+  const getMedia = async () => {
+    setStream(await navigator.mediaDevices.getUserMedia({ video: true }));
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
   return (
-    <body>
+    <>
       <header>
         <h1>Hello, world</h1>
       </header>
-    
+
       <main>
+        <video
+          id='localVideo'
+          autoPlay
+          muted
+          playsInline
+          ref={(video) => {
+            if (video && stream) {
+              video.srcObject = stream;
+            }
+          }}
+        ></video>
+        <video id='remoteVideo' autoPlay playsInline></video>
       </main>
-    </body>
+    </>
   );
 }
 

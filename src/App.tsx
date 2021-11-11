7c23de7b1ee1d581ebcac7ca6e0a9083585fa841
Christian Eberling <christian.eberling@logmein.com>
2021-11-11 13:09:38 +0100

    create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <header>
+        <h1>Hello, world</h1>
+      </header>
+    
+      <main>
+      </main>
+    </body>
+  );
+}
+
+export default App;
